29180a2d57dd92667fe5a5a53a36fa0a8730484c
RickXie <rickxie7@gmail.com>
2019-07-26 13:43:02 +1000

    few constructors + stream <<

--- client.cpp
+++ client.cpp
@@ -6,31 +6,50 @@
 // Note: At the moment, there is no client.sampleout. Please do your own testing
 
 int main() {
-  gdwg::Graph<std::string, int> g;
-  g.InsertNode("hello");
-  g.InsertNode("how");
-  g.InsertNode("are");
-  g.InsertNode("you?");
+  std::vector<std::string> v{"a", "b", "c", "d"};
+  gdwg::Graph<std::string, int> a{v.begin(), v.end()};
 
-  g.InsertEdge("hello", "how", 5);
-  g.InsertEdge("hello", "are", 8);
-  g.InsertEdge("hello", "are", 2);
+  std::cout << a << '\n';
 
-  g.InsertEdge("how", "you?", 1);
-  g.InsertEdge("how", "hello", 4);
+  std::string s1{"apple"};
+  std::string s2{"orange"};
+  auto e1 = std::make_tuple(s1, s2, 5.4);
+  auto e = std::vector<std::tuple<std::string, std::string, double>>{e1};
+  gdwg::Graph<std::string, double> b{e.begin(), e.end()};
 
-  g.InsertEdge("are", "you?", 3);
+  std::cout << b << '\n';
 
-  std::cout << g << '\n';
+  gdwg::Graph<char, std::string> c{'a', 'b', 'x', 'y'};
+  std::cout << c << '\n';
 
-  gdwg::Graph<std::string, int> g2{g};
+  gdwg::Graph<std::string, double> d{b};
+  std::cout << d << '\n';
 
-  std::cout << g2 << "\n";
 
-  // This is a structured binding.
-  // https://en.cppreference.com/w/cpp/language/structured_binding
-  // It allows you to unpack your tuple.
-  for (const auto& [from, to, weight] : g) {
-    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-  }
+  // g.InsertNode("hello");
+  // g.InsertNode("how");
+  // g.InsertNode("are");
+  // g.InsertNode("you?");
+
+  // g.InsertEdge("hello", "how", 5);
+  // g.InsertEdge("hello", "are", 8);
+  // g.InsertEdge("hello", "are", 2);
+
+  // g.InsertEdge("how", "you?", 1);
+  // g.InsertEdge("how", "hello", 4);
+
+  // g.InsertEdge("are", "you?", 3);
+
+  // std::cout << g << '\n';
+
+  // gdwg::Graph<std::string, int> g2{g};
+
+  // std::cout << g2 << "\n";
+
+  // // This is a structured binding.
+  // // https://en.cppreference.com/w/cpp/language/structured_binding
+  // // It allows you to unpack your tuple.
+  // for (const auto& [from, to, weight] : g) {
+  //   std::cout << from << " -> " << to << " (weight " << weight << ")\n";
+  // }
 }
--- graph.h
+++ graph.h
@@ -1,6 +1,21 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <algorithm>
+#include <cassert>
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <math.h>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+#include <tuple>
+
+
 namespace gdwg {
 
 template <typename N, typename E>
@@ -8,7 +23,81 @@ class Graph {
  public:
   class const_iterator {};
 
+//Constructors & Destructors
+    Graph<N, E>() {}
+    	
+    Graph<N, E>(typename std::vector<N>::const_iterator begin, 
+                typename std::vector<N>::const_iterator end
+        ) {
+            for (auto it = begin; it != end; ++it) {
+                auto node_tmp = std::make_unique <N[]>(1);
+                node_tmp[0] = *it;
+                node_.push_back(std::move(node_tmp));
+        }
+    }
+
+    Graph<N, E>(
+        typename std::vector<std::tuple<N, N, E>>::const_iterator begin,
+        typename std::vector<std::tuple<N, N, E>>::const_iterator end
+        ) {
+            for (auto it = begin; it != end; ++it) {
+                auto node_tmp_0 = std::make_unique <N[]>(1);
+                auto node_tmp_1 = std::make_unique <N[]>(1);
+                node_tmp_0[0] = std::get<0>(*it);
+                node_tmp_1[0] = std::get<1>(*it);
+                node_.push_back(std::move(node_tmp_0));
+                node_.push_back(std::move(node_tmp_1));
+                // std::unique_ptr<E[]> edge_tmp = std::make_unique <E[]>(3);
+                // edge_tmp[0] = std::get<0>(*it);
+                // edge_tmp[1] = std::get<1>(*it);
+                // edge_tmp[2] = std::get<2>(*it);
+                auto edge_tmp = std::make_unique <std::tuple<N, N, E>[]>(1);
+                edge_tmp[0] = *it;
+                edge_.push_back(std::move(edge_tmp));
+            }
+        }
+
+    Graph<N, E>(typename std::initializer_list<N> source) {
+        for (auto it = source.begin(); it != source.end(); ++it) {
+            auto node_tmp = std::make_unique <N[]>(1);
+                node_tmp[0] = *it;
+                node_.push_back(std::move(node_tmp));
+        }
+    }
+
+    Graph<N, E>(const gdwg::Graph<N, E>& source) {
+        for (auto it = source.node_.begin(); it != source.node_.end(); ++it) {
+            auto node_tmp = std::make_unique <N[]>(1);
+            node_tmp[0] = (*it)[0];
+            node_.push_back(std::move(node_tmp));
+        }
+        for (auto it = source.edge_.begin(); it != source.edge_.end(); ++it) {
+            auto edge_tmp = std::make_unique <std::tuple<N, N, E>[]>(1);
+            edge_tmp[0] = (*it)[0];
+            edge_.push_back(std::move(edge_tmp));
+        }
+    }
+
+    // Graph<N, E>(gdwg::Graph<N, E>&& source)
+
+
+    friend std::ostream& operator<<(std::ostream& os, const gdwg::Graph<N, E>& g){
+        for (auto it_n = g.node_.begin(); it_n != g.node_.end(); ++it_n) {
+            os << (*it_n)[0] << "(\n"; 
+            for (auto it_e = g.edge_.begin(); it_e != g.edge_.end(); ++it_e) {
+                if ((*it_n)[0] == std::get<0>((*it_e)[0])) {
+                    os << "  " << std::get<1>((*it_e)[0]) << " | " << std::get<2>((*it_e)[0]);
+                }
+            }
+            os << "\n)\n";
+        }
+        return os;
+    }
+
+
  private:
+    std::vector <std::unique_ptr<N[]>> node_;
+    std::vector <std::unique_ptr<std::tuple<N, N, E>[]>> edge_;
 };
 
 }  // namespace gdwg
